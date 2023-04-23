13722624ae28c900ecf51d3e50736f441b1ceee3
Horia <horia.marinescu12@gmail.com>
2023-04-23 16:51:57 +0300

    Etapa 4 - Finished

--- index.js
+++ index.js
@@ -1,14 +1,77 @@
-const express = require('express');
+const express = require("express");
+const fs= require('fs');
 
-App = express();
+obGlobal={
+    obErori:null,
+    obImagini:null
+}
 
-// console.log("Folder", _dirname);
+app= express();
+console.log("Folder proiect", __dirname);
 
-App.get("/index.html", function(req, res) {
-    res.sendFile(__dirname = "./index.html");
-    console.log("a");
-});
+app.set("view engine","ejs");
 
-App.listen(5500);
+app.use("/resources", express.static(__dirname+"/resources"));
 
-console.log("Server listening on port 5500")
+app.get("/ceva", function(req, res){
+    console.log("cale:",req.url)
+    res.send("<h1>altceva</h1> ip:"+req.ip);
+})
+
+
+
+app.get(["/index","/","/home" ], function(req, res){
+    res.render("pagini/index" , {ip: req.ip, a: 10, b:20});
+})
+
+
+app.get("/*",function(req, res){
+    res.render("pagini"+req.url, function(err, rezRandare){
+        if(err){
+            console.log(err);
+            if(err.message.startsWith("Failed to lookup view"))
+                afisareEroare(res,404);
+            else
+                afisareEroare(res);
+        }
+        else{
+            console.log(rezRandare);
+            res.send(rezRandare);
+        }
+    } );
+})
+
+function initErori(){
+    var continut= fs.readFileSync(__dirname+"/resources/json/erori.json").toString("utf-8");
+    // console.log(continut);
+    obGlobal.obErori=JSON.parse(continut);
+    let vErori=obGlobal.obErori.info_erori;
+    //for (let i=0; i< vErori.length; i++ )
+    for (let eroare of vErori){
+        eroare.imagine="/"+obGlobal.obErori.cale_baza+"/"+eroare.imagine;
+    }
+}
+initErori();
+
+function afisareEroare(res, _identificator, _titlu, _text, _imagine){
+    let vErori=obGlobal.obErori.info_erori;
+    let eroare=vErori.find(function(elem) {return elem.identificator==_identificator;} )
+    if(eroare){
+        let titlu1= _titlu || eroare.titlu;
+        let text1= _text || eroare.text;
+        let imagine1= _imagine || eroare.imagine;
+        if(eroare.status)
+            res.status(eroare.identificator).render("pagini/eroare", {titlu:titlu1, text:text1, imagine:imagine1});
+        else
+            res.render("pagini/eroare", {titlu:titlu1, text:text1, imagine:imagine1});
+    }
+    else{
+        let errDef=obGlobal.obErori.eroare_default;
+        res.render("pagini/eroare", {titlu:errDef.titlu, text:errDef.text, imagine:errDef.imagine});
+    }
+}
+
+port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log("Server listening on port", port, "...");
